02d5b9af4b4bab8bf4e6d712476f135061c50de1
Stefan Ruvceski <stefanruvceski@gmail.com>
2021-04-04 19:09:11 +0200

    SPA 1

--- confusion2/src/components/MainComponent.js
+++ confusion2/src/components/MainComponent.js
@@ -1,10 +1,13 @@
 import React, { Component} from 'react';
 import Home from './HomeComponent'
 import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import {Switch,Route,Redirect} from 'react-router-dom'
 
 
@@ -15,7 +18,10 @@ class Main extends Component {
         super(props);
 
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -23,7 +29,11 @@ class Main extends Component {
     render() {
         const HomePage = () =>{
             return(
-                <Home/>
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((lead) => lead.featured)[0]}
+                />
             );
         }
         return (
@@ -31,6 +41,7 @@ class Main extends Component {
             <Header/>
                 <Switch>
                     <Route path="/home" component={HomePage} />
+                    <Route exact path="/contact-us" component={Contact} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
                     <Redirect to='/home' />
                 </Switch>
