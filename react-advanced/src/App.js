103e00251ce5a913e9aeb6d524836bdcd2a3974e
Stefan Ruvceski <stefanruvceski@gmail.com>
2021-04-09 12:51:01 +0200

    react route

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/11-react-router/setup";
 import Project from "./tutorial/8-useContext/project";
 import Final from "./tutorial/1-useState/final/5-useState-counter";
 function App() {
   return (
     <div className="container">
-      {/* <Setup /> */}
-      <Project />
+      <Setup />
+      {/* <Project /> */}
       {/* <Final /> */}
     </div>
   );
--- react-advanced/src/tutorial/11-react-router/setup/Person.js
+++ react-advanced/src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const id = parseInt(useParams().id);
+  const person = data.filter((p) => p.id === id)[0];
   return (
     <div>
-      <h2>person</h2>
+      <h1>{person.name}</h1>
+      <Link to="/people" className="btn">
+        Back
+      </Link>
     </div>
   );
 };
